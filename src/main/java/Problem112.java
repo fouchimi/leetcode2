2c3069cfa9a809158797d1b7e3ebb9c0546f7f87
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2019-10-03 23:52:25 -0600

    Completed problem112 Path Sum

---
+++ src/main/java/Problem112.java
@@ -0,0 +1,21 @@
+public class Problem112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null && sum == root.val) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public static void main(String[] args) {
+        Problem112 problem112 = new Problem112();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+        System.out.println(problem112.hasPathSum(root, 22));
+    }
+}
