bf28eabec4f5668c9db8f2a08af341bdf71ae387
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2019-10-03 21:35:54 -0600

    Completed problem99 Validate binary search tree

---
+++ src/main/java/Problem99.java
@@ -0,0 +1,24 @@
+public class Problem99 {
+
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean dfs(TreeNode x, long min, long max) {
+        if (x == null) return true;
+        long v = x.val;
+        return (v > min && v < max) && dfs(x.left, min, v) && dfs(x.right, v, max);
+    }
+
+    public static void main(String[] args) {
+        Problem99 problem99 = new Problem99();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(6);
+
+        System.out.println(problem99.isValidBST(root));
+    }
+}
