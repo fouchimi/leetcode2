2d4588cf9450a5c5683d2c635aa9ecf0d5e19bb7
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2019-10-24 08:57:39 -0600

    Completed Problem130 Removed Invalid Parentheses

---
+++ src/main/java/Problem310.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Problem310 {
+
+    List<String> results;
+    Set<String> set;
+
+    public List<String> removeInvalidParentheses(String s) {
+        if (s.length() == 0) return Arrays.asList("");
+
+        results = new ArrayList<>();
+        set = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.add(s);
+
+        int foundLength = 0;
+        while (!q.isEmpty()) {
+            String input = q.poll();
+
+            if (input.length() < foundLength) break;
+
+            if (set.contains(input)) continue;
+
+            set.add(input);
+            if (isValid(input)) {
+                results.add(input);
+                foundLength = input.length();
+            } else { /* if we find what we need we stop checking for smaller lengths */
+                if (input.length() - 1 >= foundLength) {
+                    for (int i = 0; i < input.length(); i++) {
+                        q.add(input.substring(0,i) + input.substring(i+1));
+                    }
+                }
+            }
+        }
+
+        return results;
+    }
+
+    /**
+     Complexity O(n)
+     */
+    private boolean isValid(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+                continue;
+            }
+
+            if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Problem310 problem310 = new Problem310();
+        List<String> list = problem310.removeInvalidParentheses("()())()");
+        for(String parenthese : list) {
+            System.out.print(parenthese + " ");
+        }
+    }
+}
