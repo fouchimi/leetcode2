c6f9bc9644ad727c854729dbe09715b64c5eea5a
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2019-12-17 08:29:53 -0700

    Started problem96

--- src/main/java/Problem96.java
+++ src/main/java/Problem96.java
@@ -1,13 +1,29 @@
 public class Problem96 {
     public int numTrees(int n) {
         if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        if (n % 2 == 0) {
-
-        } else {
-
+        else if (n == 1) return 1;
+        else if (n == 2) return 2;
+        else {
+            int res = 0, mid = n / 2;
+            if (n % 2 == 0) {
+                for (int i = 0; i <= mid - 1; i++) {
+                    res += n - 1;
+                    n--;
+                }
+                return 2 * res;
+            } else {
+                for (int i = 0; i < mid; i++) {
+                    res += n - 1;
+                    n--;
+                }
+                return 2 * res + 1;
+            }
         }
-        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem96 problem96 = new Problem96();
+        System.out.println(problem96.numTrees(3));
+        System.out.println(problem96.numTrees(4));
     }
 }
