7e6dcbf5ed327037debbfa4019b50e25d5580e3d
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-01-09 09:58:09 -0700

    Completed problem152 Maximum Product Subarray

--- src/main/java/Problem152.java
+++ src/main/java/Problem152.java
@@ -1,29 +1,31 @@
 public class Problem152 {
     public int maxProduct(int[] nums) {
-        int p = 0, maxProduct = Integer.MIN_VALUE;
-        while (p < nums.length) {
-            int q = p + 1, product = nums[p];
-            while (q < nums.length  && nums[q] == nums[q-1] + 1) {
-                product = Math.max(product, product * nums[q]);
-                q++;
+        int r = nums[0];
+
+        for (int i = 1, imin = r, imax = r; i < nums.length; i++) {
+
+            if (nums[i] < 0) {
+                int temp = imax;
+                imax = imin;
+                imin = temp;
             }
-            while (q < nums.length && nums[p] == nums[q]) {
-                product = Math.max(product, product * nums[q]);
-                q++;
-            }
-            maxProduct = Math.max(product, maxProduct);
-            p = q;
+
+            imax = Math.max(nums[i], nums[i] * imax);
+            imin = Math.min(nums[i], nums[i] * imin);
+
+            r = Math.max(r, imax);
         }
-        return maxProduct;
+
+        return r;
     }
 
     public static void main(String[] args) {
         Problem152 problem152 = new Problem152();
         System.out.println(problem152.maxProduct(new int[]{2, 3, -2, 4}));
-        System.out.println(problem152.maxProduct(new int[]{-2,0,-1}));
+        System.out.println(problem152.maxProduct(new int[]{-2, 0, -1}));
         System.out.println(problem152.maxProduct(new int[]{-4, -3}));
         System.out.println(problem152.maxProduct(new int[]{-1, -1}));
-        System.out.println(problem152.maxProduct(new int[]{-4,-3,-2}));
+        System.out.println(problem152.maxProduct(new int[]{-4, -3, -2}));
         System.out.println(problem152.maxProduct(new int[]{-2, 3, -4}));
     }
 }
