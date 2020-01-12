69d6d27527b0575c19dc76d815194f0e531bb06f
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-01-12 16:41:33 -0700

    Started problem385

---
+++ src/main/java/Problem385.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class Problem385 {
+    public List<Object> deserialize(String s) {
+        Stack<Object> stack = new Stack<>();
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(2);
+        Iterator<Integer> it = list.iterator();
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+    }
+
+}
