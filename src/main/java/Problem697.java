65f8ddf0d72fa2af34a545c16e47cd1f40d9502d
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-04-25 08:54:18 -0600

    Completed problem697 Degree of an Array

--- src/main/java/Problem697.java
+++ src/main/java/Problem697.java
@@ -24,7 +24,6 @@ public class Problem697 {
                 result = Math.min(result, (maxIndexMap.get(key) - minIndexMap.get(key) + 1));
             }
         }
-
         return result;
     }
 
