cb09a54f640faac38345cadca8762a4a41108dcc
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-05-16 11:46:43 -0600

    Started problem140

--- src/main/java/Problem140.java
+++ src/main/java/Problem140.java
@@ -3,32 +3,59 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Problem140 {
+
+    static class TrieNode {
+        String value;
+        TrieNode[] child = new TrieNode[256];
+        public TrieNode() { }
+    }
+
+    TrieNode root = new TrieNode();
+
     public List<String> wordBreak(String s, List<String> wordDict) {
         List<String> res = new ArrayList<>();
-        helper(s, wordDict, "", res);
+        for (String word : wordDict) insert(word);
+        dfs(s, res);
         return res;
     }
 
-    private void helper(String s, List<String> wordDict, String word,  List<String> res) {
-        if (s.length() == 0) {
-            res.add(word.trim());
-            return;
+    public void insert(String word) {
+        root = insert(root, word, 0);
+    }
+
+    private TrieNode insert(TrieNode x, String word, int d) {
+        if (x == null) x = new TrieNode();
+        if (d == word.length()) {
+            x.value = word;
+            return x;
         }
-        for (int i = 0; i < wordDict.size(); i++) {
-            String currentWord = wordDict.get(i);
-            boolean found = false;
-            String tempStr = s, tempWord = word;
-            while (s.length() >= currentWord.length() && s.startsWith(currentWord)) {
-                s = s.substring(currentWord.length());
-                word += currentWord + " ";
-                found = true;
-            }
-            if (found) {
-                helper(s, wordDict, word, res);
-                word = tempWord;
-                s = tempStr;
-            }
+        int index = word.charAt(d);
+        x.child[index] = insert(x.child[index], word, d + 1);
+        return x;
+    }
+
+    private void dfs (String needle, List<String> res) {
+        root = dfs(root, needle, new ArrayList<>(), res, 0);
+    }
+
+    private TrieNode dfs (TrieNode x, String word, List<String> tempList, List<String> res, int d) {
+        if (x == null) return null;
+        if (word.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (String key : tempList) sb.append(key).append(" ");
+            res.add(sb.toString().trim());
+            return x;
         }
+        if (x.value != null) {
+            tempList.add(x.value);
+            int startIndex = x.value.length();
+            dfs(root, word.substring(startIndex), tempList, res, 0);
+            tempList.remove(tempList.size() - 1);
+        }
+        if (word.length() == d) return x;
+        int index = word.charAt(d);
+        x.child[index] = dfs(x.child[index], word, tempList, res, d + 1);
+        return x;
     }
 
     public static void main(String[] args) {
@@ -36,5 +63,6 @@ public class Problem140 {
         String s = "aaaaaaa";
         List<String> wordDict = Arrays.asList("aaaa", "aaa");
         System.out.println(problem140.wordBreak(s, wordDict));
+        System.out.println(problem140.wordBreak("catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")));
     }
 }
