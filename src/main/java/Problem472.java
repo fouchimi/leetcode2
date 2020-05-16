7b5884ea6293acadd580aa932b3d102b7955c0fe
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-05-16 02:24:59 -0600

    Completed problem472 Concatenated Words

--- src/main/java/Problem472.java
+++ src/main/java/Problem472.java
@@ -1,54 +1,105 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Problem472 {
+
     static class TrieNode {
-        char ch;
         String value;
         private TrieNode[] children = new TrieNode[256];
         TrieNode() { }
     }
+
     private TrieNode root = new TrieNode();
+
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Arrays.sort(words, (e1, e2) -> {
+            if (e1.length() < e2.length()) return -1;
+            else if (e1.length() > e2.length()) return 1;
+            return 0;
+        });
+
         List<String> res = new ArrayList<>();
         for (String word : words) {
-            List<String> l = new ArrayList<>();
-            findOrInsert(word, l);
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < l.size(); i++) sb.append(l.get(i));
-            if (sb.toString().equals(word)) res.add(word);
+            if (word.isEmpty()) continue;
+            if (hasPrefix(word)) {
+                if (isConcatenatedWord(word)) {
+                    res.add(word);
+                }
+            }
+            insert(word);
         }
         return res;
     }
 
-    private TrieNode findOrInsert(String word, List<String> list) {
-        return findOrInsert(root, word, list, 0);
+    private void insert(String word) {
+        root = insert(root, word, 0);
     }
 
-    private TrieNode findOrInsert(TrieNode x, String word, List<String> list, int d) {
+    private TrieNode insert(TrieNode x, String word, int d) {
         if (x == null) x = new TrieNode();
-        if (x.value != null) {
-            int index = word.indexOf(x.value);
-            if (index > -1) {
-                list.add(x.value);
-            }
-        }
         if (d == word.length()) {
             x.value = word;
             return x;
         }
-        x.ch = word.charAt(d);
-        int charIndex = x.ch;
-        x.children[charIndex] = findOrInsert(x.children[charIndex], word, list, d+1);
+        int index = word.charAt(d);
+        x.children[index] = insert(x.children[index], word, d + 1);
         return x;
+    }
+
+    private boolean hasPrefix(String word) {
+        return hasPrefix(root, word, 0);
+    }
+
+    private boolean hasPrefix(TrieNode x, String word, int d) {
+        if (x == null) return false;
+        if (x.value != null) return true;
+        int index = word.charAt(d);
+        return hasPrefix(x.children[index], word, d + 1);
+    }
+
+    private boolean isConcatenatedWord(String word) {
+        if (word.length() == 0) return true;
+        List<String> prefixList = getPrefixList(word);
+        for (String prefix : prefixList) {
+            int startIndex = prefix.length();
+            String suffix = word.substring(startIndex);
+            if (isFound(suffix) || isConcatenatedWord(suffix)) return true;
+        }
+        return false;
+    }
+
+    private List<String> getPrefixList(String word) {
+        List<String> prefixList = new ArrayList<>();
+        root = getPrefixList(root, word, prefixList, 0);
+        return prefixList;
+    }
+
+    private TrieNode getPrefixList(TrieNode x, String word, List<String> prefixList, int d) {
+        if (x == null) return null;
+        if (d == word.length()) return x;
+        if (x.value != null) prefixList.add(x.value);
+        int index = word.charAt(d);
+        x.children[index] = getPrefixList(x.children[index], word, prefixList, d + 1);
+        return x;
+    }
+
+    private boolean isFound(String word) {
+        return isFound(root, word, 0);
+    }
+
+    private boolean isFound(TrieNode x, String word, int d) {
+        if (x == null) return false;
+        if (d == word.length()) {
+            if (x.value == null) return false;
+            return true;
+        }
+        int index = word.charAt(d);
+        return isFound(x.children[index], word, d + 1);
     }
 
     public static void main(String[] args) {
         Problem472 problem472 = new Problem472();
         String[] words = {"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"};
-        //String[] words = {"cat","dog","catdog"};
+//        String[] words = {"cat","dog","catdog"};
         List<String> res = problem472.findAllConcatenatedWordsInADict(words);
         System.out.println(res);
     }
