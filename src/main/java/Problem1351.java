d1fd7d89186ae819dac643466dc5cd5d2bbfe08b
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-05-25 23:26:38 -0600

    Completed problem1351 Count Negative Numbers in a Sorted Matrix

---
+++ src/main/java/Problem1351.java
@@ -0,0 +1,12 @@
+public class Problem1351 {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[0].length - 1; j >= 0; j--) {
+                if (grid[i][j] >= 0) break;
+                else count++;
+            }
+        }
+        return count;
+    }
+}
