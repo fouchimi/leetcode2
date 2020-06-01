465be0aa77c8bdb867cce3a70b8e26eba08d954f
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-06-01 11:46:34 -0600

    Updated problem1267

--- src/main/java/Problem1267.java
+++ src/main/java/Problem1267.java
@@ -6,39 +6,54 @@ public class Problem1267 {
     public int countServers(int[][] grid) {
         int N = grid.length, M = grid[0].length;
         int size = Math.max(N, M);
-        List<Integer>[] list = new ArrayList[size];
+        List<Integer>[] map = new ArrayList[size];
 
-        for (int i = 0; i < size; i++) list[i] = new ArrayList<>();
+        for (int i = 0; i < size; i++) map[i] = new ArrayList<>();
 
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < M; col++) {
+                if (grid[row][col] == 1) map[row].add(col);
+            }
+        }
+
+        for (int col = 0; col < M; col++) {
+            for (int row = 0; row < N; row++) {
                 if (grid[row][col] == 1) {
-                    list[row].add(col);
-                    list[col].add(row);
+                   if (!map[col].contains(row)) map[col].add(row);
                 }
             }
         }
 
-        boolean[] visited = new boolean[size];
-        int[] count = new int[1];
+        boolean[][] visited = new boolean[N][M];
+        int ans = 0;
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < M; col++) {
+                if (!visited[row][col] && grid[row][col] == 1) {
 
-        for (int v = 0; v < size; v++) {
-            if (!visited[v] && !list[v].isEmpty()) {
-                dfs(v, visited, list, count);
+                }
             }
         }
 
-        return 0;
+        return ans;
     }
 
-    private void dfs(int s, boolean[] visited, List<Integer>[] list, int[] count) {
-        if (!visited[s]) count[0]++;
-        visited[s] = true;
-        for (int w : list[s]) {
-            if (!visited[w]) {
-                dfs(w, visited, list, count);
+    private void dfs(int u, int v,  List<Integer>[] map, boolean[][] visited, int[] count) {
+        count[0]++;
+        for (int w : map[u]) {
+
+            if (!visited[u][w]) {
+                dfs(u, w, map, visited, count);
             }
         }
     }
+
+    public static void main(String[] args) {
+        Problem1267 problem1267 = new Problem1267();
+        System.out.println(problem1267.countServers(new int[][]{{1, 0}, {0, 1}}));
+        System.out.println(problem1267.countServers(new int[][]{{1, 0}, {1, 1}}));
+        System.out.println(problem1267.countServers(new int[][]{{1, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}));
+        System.out.println(problem1267.countServers(new int[][]{{1, 0, 0, 1, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+    }
+
 
 }
