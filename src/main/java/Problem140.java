cf9323bfbd0564862aadbd583be3f142be0f2014
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-06-02 23:22:53 -0600

    Completed problem140 Word Break

--- src/main/java/Problem140.java
+++ src/main/java/Problem140.java
@@ -2,76 +2,154 @@ import java.util.*;
 
 public class Problem140 {
 
-    static class TrieNode {
-        String value;
-        TrieNode[] child = new TrieNode[256];
-        public TrieNode() { }
-    }
+//    static class TrieNode {
+//        String value;
+//        TrieNode[] child = new TrieNode[256];
+//        public TrieNode() { }
+//    }
+//
+//    TrieNode root = new TrieNode();
+//
+//    public List<String> wordBreak(String s, List<String> wordDict) {
+//        List<String> res = new ArrayList<>();
+//        for (String word : wordDict) insert(word);
+//        Queue<List<List<String>>> queue = new LinkedList<>();
+//        List<String> initialList = new ArrayList<>();
+//        initialList.add(s);
+//        List<List<String>> list = new ArrayList<>();
+//        list.add(initialList);
+//        queue.offer(list);
+//        List<List<String>> resultList = new ArrayList<>();
+//        while (!queue.isEmpty()) {
+//            List<List<String>> currentList = queue.poll();
+//            List<List<String>> tempCurrentList = new ArrayList<>();
+//            for(List<String> curList : currentList) {
+//                String key = curList.remove(0);
+//                if (key.isEmpty()) {
+//                    resultList.add(curList);
+//                    continue;
+//                }
+//                List<String> prefixes = getPrefixes(key, 0);
+//                if (prefixes.size() > 0) {
+//                    for (String prefix : prefixes) {
+//                        List<String> tempList = new ArrayList<>(curList);
+//                        String newKey = key.substring(prefix.length());
+//                        tempList.add(0, newKey);
+//                        tempList.add(prefix);
+//                        tempCurrentList.add(tempList);
+//                    }
+//                }
+//            }
+//            if(!tempCurrentList.isEmpty()) queue.offer(tempCurrentList);
+//        }
+//        for (List<String> l : resultList) {
+//            StringBuilder sb = new StringBuilder();
+//            for (String w : l) sb.append(w).append(" ");
+//            res.add(sb.toString().trim());
+//        }
+//        return res;
+//    }
+//
+//    public void insert(String word) {
+//        root = insert(root, word, 0);
+//    }
+//
+//    private TrieNode insert(TrieNode x, String word, int d) {
+//        if (x == null) x = new TrieNode();
+//        if (d == word.length()) {
+//            x.value = word;
+//            return x;
+//        }
+//        int index = word.charAt(d);
+//        x.child[index] = insert(x.child[index], word, d + 1);
+//        return x;
+//    }
+//
+//    private List<String> getPrefixes(String word, int d) {
+//        List<String> prefixes = new ArrayList<>();
+//        root = getPrefixes(root, word, d, prefixes);
+//        return prefixes;
+//    }
+//
+//    private TrieNode getPrefixes(TrieNode x, String word, int d, List<String> prefixes) {
+//        if (x == null) return null;
+//        if (x.value != null) prefixes.add(x.value);
+//        if (d == word.length()) return x;
+//        int index = word.charAt(d);
+//        x.child[index] = getPrefixes(x.child[index], word, d + 1, prefixes);
+//        return x;
+//    }
 
-    TrieNode root = new TrieNode();
 
+    List<String> results;
+    List<String> wordDict;
+    Map<String, List<List<String>>> cache;
     public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> res = new ArrayList<>();
-        for (String word : wordDict) insert(word);
-        dfs(s, res);
-        return res;
-    }
+        this.wordDict = wordDict;
+        this.results = new ArrayList<>();
+        this.cache = new HashMap<>();
+        for (String w: wordDict) {
+            if (s.startsWith(w)) {
+                List<List<String>> lists = dfs(s.substring(w.length()));
+                if (!lists.isEmpty()) {
+                    for (List<String> l: lists) {
+                        l.add(w);
+                    }
+                }
 
-    public void insert(String word) {
-        root = insert(root, word, 0);
-    }
+                for (List<String> l: lists) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = l.size()-1; i>=0; i--) {
+                        String word = l.get(i);
+                        sb.append(word).append(" ");
+                    }
+                    results.add(sb.substring(0, sb.length()-1));
+                }
 
-    private TrieNode insert(TrieNode x, String word, int d) {
-        if (x == null) x = new TrieNode();
-        if (d == word.length()) {
-            x.value = word;
-            return x;
-        }
-        int index = word.charAt(d);
-        x.child[index] = insert(x.child[index], word, d + 1);
-        return x;
-    }
-
-    private void dfs (String needle, List<String> res) {
-        Map<String, Boolean> map = new HashMap<>();
-        dfs(root, needle, new ArrayList<>(), res, 0, map);
-    }
-
-    private boolean dfs (TrieNode x, String word, List<String> tempList, List<String> res, int d, Map<String, Boolean> map) {
-        if (x == null) return false;
-        if (map.containsKey(word)) {
-            System.out.println("Hi there");
-            tempList.add(word);
-            int startIndex = word.length();
-            dfs(root, word.substring(startIndex), tempList, res, 0, map);
-            return map.get(word);
-        }
-        if (word.isEmpty()) {
-            StringBuilder sb = new StringBuilder();
-            for (String key : tempList) sb.append(key).append(" ");
-            res.add(sb.toString().trim());
-            return true;
-        }
-        if (x.value != null) {
-            tempList.add(x.value);
-            int startIndex = x.value.length();
-            if (dfs(root, word.substring(startIndex), tempList, res, 0, map)) {
-                map.put(word, true);
             }
-            tempList.remove(tempList.size() - 1);
+        }
+        return results;
+    }
+
+    public List<List<String>> dfs(String s) {
+        if (cache.containsKey(s)) {
+            return cache.get(s);
         }
 
-        if (word.length() == d) return x.value != null;
+        if (s.length() == 0) {
+            List<String> list = new ArrayList<>();
+            List<List<String>> result = new ArrayList<>();
+            result.add(list);
+            return result;
+        }
 
-        int index = word.charAt(d);
-        return dfs(x.child[index], word, tempList, res, d + 1, map);
+        List<List<String>> current = new ArrayList<>();
+        for (String w: wordDict) {
+            if (s.startsWith(w)) {
+                List<List<String>> lists = dfs(s.substring(w.length()));
+                List<List<String>> clone = new ArrayList<>();
+                for (List<String> origin: lists) {
+                    clone.add(new ArrayList<>(origin));
+                }
+                cache.put(s.substring(w.length()), clone);
+                if (lists.size() != 0) {
+                    for (List<String> l: lists) {
+                        l.add(w);
+                        current.add(l);
+                    }
+                }
+
+            }
+        }
+        return current;
     }
 
     public static void main(String[] args) {
         Problem140 problem140 = new Problem140();
-        String s = "aaaaaaa";
-        List<String> wordDict = Arrays.asList("aaaa", "aaa");
-        System.out.println(problem140.wordBreak(s, wordDict));
+        System.out.println(problem140.wordBreak("aaaaaaa", Arrays.asList("aaaa", "aaa")));
         System.out.println(problem140.wordBreak("catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")));
+        System.out.println(problem140.wordBreak("pineapplepenapple", Arrays.asList("apple", "pen", "applepen", "pine", "pineapple")));
+//        System.out.println(problem140.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+//                Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa")));
     }
 }
