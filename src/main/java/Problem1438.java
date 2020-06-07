8df9808bc1e0c111a3294d25708a1e94efb61a5a
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-06-07 17:47:38 -0600

    Updated with alternative solution

--- src/main/java/Problem1438.java
+++ src/main/java/Problem1438.java
@@ -20,6 +20,24 @@ public class Problem1438 {
         return res;
     }
 
+//    public int longestSubarray(int[] nums, int limit) {
+//        int i = 0, j = 0, min = nums[0], max = nums[0], ans = 0;
+//        while (j < nums.length) {
+//            min = Math.min(min, nums[j]);
+//            max = Math.max(max, nums[j]);
+//            if (Math.abs(max - min) <= limit) ans = Math.max(ans, j - i + 1);
+//            else {
+//                i++;
+//                while (i > 0 && nums[i - 1] == nums[i]) i++;
+//                min = nums[i];
+//                max = nums[i];
+//                j = i;
+//            }
+//            j++;
+//        }
+//        return ans;
+//    }
+
     public static void main(String[] args) {
         Problem1438 problem1438 = new Problem1438();
         System.out.println(problem1438.longestSubarray(new int[]{8, 2, 4, 7}, 4));
