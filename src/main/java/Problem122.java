1bedf9599698b7e964202b0d66c1d83a7f59ff29
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-06-13 15:01:56 -0600

    Updated code

--- src/main/java/Problem122.java
+++ src/main/java/Problem122.java
@@ -2,7 +2,9 @@ public class Problem122 {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
-            maxProfit = Math.max(maxProfit, prices[i] - prices[i-1]);
+            if (prices[i] - prices[i-1] > 0) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
         }
         return maxProfit;
     }
