3e6d1b3198e402b28de299dc369c2a65ec78f66e
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-08-20 01:25:36 -0600

    Completed problem543 Diameter of Binary Tree

--- src/main/java/Problem543.java
+++ src/main/java/Problem543.java
@@ -1,32 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem543 {
-    private int left = 0, right = 0;
     public int diameterOfBinaryTree(TreeNode root) {
         if (root == null) return 0;
-        getLeft(root.left, 1);
-        getRight(root.right, 1);
-        return Math.max(Math.max(left + right, left), right);
+        List<TreeNode> list = new ArrayList<>();
+        dfs(root, list);
+        int maxDepth = 0;
+        for (TreeNode x : list) {
+            int left = getDepth(x.left);
+            int right = getDepth(x.right);
+            maxDepth = Math.max(maxDepth, left + right);
+        }
+        return maxDepth;
     }
 
-    private void getLeft(TreeNode x, int d) {
+    private void dfs(TreeNode x, List<TreeNode> list) {
         if (x == null) return;
-        if (d > left) left = d;
-        getLeft(x.left, d + 1);
-        getLeft(x.right, d + 1);
+        dfs(x.left, list);
+        list.add(x);
+        dfs(x.right, list);
     }
 
-    private void getRight(TreeNode x, int d) {
-        if (x == null) return;
-        if (d > right) right = d;
-        getRight(x.right, d + 1);
-        getRight(x.right, d + 1);
+    private int getDepth(TreeNode x) {
+        if (x == null) return 0;
+        int left = 0, right = 0;
+        left += getDepth(x.left) + 1;
+        right += getDepth(x.right) + 1;
+        return Math.max(left, right);
     }
 
     public static void main(String[] args) {
         Problem543 problem543 = new Problem543();
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(5);
         System.out.println(problem543.diameterOfBinaryTree(root));
     }
