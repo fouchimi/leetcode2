13b140ca72c3eea398f6d237aa65dbbe104e2f28
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2020-08-19 23:36:33 -0600

    Completed problem1267 Count Servers that communicate

--- src/main/java/Problem1267.java
+++ src/main/java/Problem1267.java
@@ -1,49 +1,64 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem1267 {
 
     public int countServers(int[][] grid) {
-        int N = grid.length, M = grid[0].length;
-        int size = Math.max(N, M);
-        List<Integer>[] map = new ArrayList[size];
-
-        for (int i = 0; i < size; i++) map[i] = new ArrayList<>();
-
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < M; col++) {
-                if (grid[row][col] == 1) map[row].add(col);
-            }
-        }
-
-        for (int col = 0; col < M; col++) {
-            for (int row = 0; row < N; row++) {
-                if (grid[row][col] == 1) {
-                   if (!map[col].contains(row)) map[col].add(row);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int res = 0, rows = grid.length, cols = grid[0].length;
+        boolean[] visited = new boolean[rows * cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    int node = convert(i, j, cols);
+                    getChildren(node, i, j, grid, map);
                 }
             }
         }
 
-        boolean[][] visited = new boolean[N][M];
-        int ans = 0;
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < M; col++) {
-                if (!visited[row][col] && grid[row][col] == 1) {
-
-                }
-            }
+        for (int key : map.keySet()) {
+            if (visited[key]) continue;
+            int[] count = new int[1];
+            dfs(key, map, count, visited);
+            if (count[0] > 1) res += count[0];
         }
 
-        return ans;
+        return res;
     }
 
-    private void dfs(int u, int v,  List<Integer>[] map, boolean[][] visited, int[] count) {
-        count[0]++;
-        for (int w : map[u]) {
+    private int convert(int row, int col, int N) {
+        return N * row + col;
+    }
 
-            if (!visited[u][w]) {
-                dfs(u, w, map, visited, count);
-            }
+    private void getChildren(int node, int row, int col, int[][] grid, Map<Integer, List<Integer>> map) {
+        map.putIfAbsent(node, new ArrayList<>());
+        int cols = grid[0].length;
+        // Go left
+        for (int c = col - 1; c >= 0; c--) {
+            if (grid[row][c] == 1) map.get(node).add(convert(row, c, cols));
+        }
+
+        // Go right
+        for (int c = col + 1; c < grid[0].length; c++) {
+            if (grid[row][c] == 1) map.get(node).add(convert(row, c, cols));
+        }
+
+        // Go up
+        for (int r = row - 1; r >= 0; r--) {
+            if (grid[r][col] == 1) map.get(node).add(convert(r, col, cols));
+        }
+
+        // Go Down
+        for (int r = row + 1; r < grid.length; r++) {
+            if (grid[r][col] == 1) map.get(node).add(convert(r, col, cols));
+        }
+    }
+
+    private void dfs(int s, Map<Integer, List<Integer>> map, int[] count, boolean[] visited) {
+        if (visited[s]) return;
+        visited[s] = true;
+        count[0]++;
+        for (int w : map.get(s)) {
+            dfs(w, map, count, visited);
         }
     }
 
@@ -53,6 +68,7 @@ public class Problem1267 {
         System.out.println(problem1267.countServers(new int[][]{{1, 0}, {1, 1}}));
         System.out.println(problem1267.countServers(new int[][]{{1, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}));
         System.out.println(problem1267.countServers(new int[][]{{1, 0, 0, 1, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+        System.out.println(problem1267.countServers(new int[][]{{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 1}, {0, 0, 1, 1}, {0, 0, 0, 1}}));
     }
 
 
